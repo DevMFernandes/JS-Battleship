87980eb4103293b4ad06b949f586388857ac7e1d
Marcos A. Fernandes <contato@marcos-fernandes.com>
2019-10-03 17:10:05 -0300

    fixed computer and player placements

--- src/js/gameboard.js
+++ src/js/gameboard.js
@@ -51,48 +51,102 @@ const gameBoard = () => {
     ships.forEach((ship, i) => {
       const len = ship.len
       let o = Math.floor(Math.random() * 2) === 0 ? 'v' : 'h'
-      let x = Math.ceil(Math.random() * setLength())
-      let y = Math.ceil(Math.random() * setLength())
-      while (!checkAvail(o, x, y, len)) {
-        o = Math.floor(Math.random() * 2) === 0 ? 'v' : 'h'
-        x = Math.ceil(Math.random() * setLength())
-        y = Math.ceil(Math.random() * setLength())
+      let x = Math.floor(Math.random() * setLength()) + 1
+      let y = Math.floor(Math.random() * setLength()) + 1
+
+      const positions = []
+      const check = () => {
+        for (let i = 0; i < positions.length; i++) {
+          const e = positions[i]
+          if (o === 'v') {
+            if (parseInt(x) + len - 1 > setLength()) {
+              return false
+            }
+            for (let i = x; i <= len + x; i++) {
+              if (e === `${i},${y}`) {
+                return false
+              }
+            }
+          } else {
+            if (parseInt(y) + len - 1 > setLength()) {
+              return false
+            }
+            for (let i = y; i <= len + y; i++) {
+              if (e === `${x},${i}`) {
+                return false
+              }
+            }
+          }
+        }
+        return true
       }
+
+      while (check() === false) {
+        if (Math.floor(Math.random() * 2) === 0) {
+          o = 'v'
+          x = Math.floor(Math.random() * setLength()) + 1
+          y = Math.floor(Math.random() * setLength()) + 1
+        } else {
+          o = 'h'
+          x = Math.floor(Math.random() * setLength()) + 1
+          y = Math.floor(Math.random() * setLength()) + 1
+        }
+      }
+
+      if (o === 'v') {
+        for (let i = x; i < len + x; i++) {
+          positions.push(`${i},${y}`)
+        }
+      } else {
+        for (let i = y; i < len + y; i++) {
+          positions.push(`${x},${i}`)
+        }
+      }
+
       placeShip(ship, `${x},${y}`, o)
     })
+    if (Object.keys(board).length !== 14) {
+      location.reload()
+    }
   }
 
   const checkAvail = (o, x, y, shipLen) => {
-    if (o === 'h') {
-      if (parseInt(y) + shipLen - 1 > setLength()) {
-        updateMsg('Ship cannot be outside of the board!')
-        return false
-      }
-      for (let i = 0; i < shipLen; i++) {
-        if (board[`${x},${parseInt(y) + i}`]) {
-          updateMsg('Cannot place ship on top of existing ship!')
-          return false
+    const positions = []
+    for (let i = 0; i < Object.keys(board).length; i++) {
+      const e = Object.keys(board)[i]
+      positions.push(e)
+    }
+    const check = (len) => {
+      for (let i = 0; i < positions.length; i++) {
+        const e = positions[i]
+        if (o === 'v') {
+          if (parseInt(x) + shipLen - 1 > setLength()) {
+            updateMsg('Ship cannot be outside of the board!')
+            return false
+          }
+          for (let i = x; i <= len + x; i++) {
+            if (e === `${i},${y}`) {
+              updateMsg('Cannot place ship on top of each other!')
+              return false
+            }
+          }
         } else {
-          updateMsg('Ship placed!')
-          return true
+          if (parseInt(y) + shipLen - 1 > setLength()) {
+            updateMsg('Ship cannot be outside of the board!')
+            return false
+          }
+          for (let i = y; i <= len + y; i++) {
+            if (e === `${x},${i}`) {
+              updateMsg('Cannot place ship on top of each other!')
+              return false
+            }
+          }
         }
       }
+      updateMsg('Ship placed!')
+      return true
     }
-    if (o === 'v') {
-      if (parseInt(x) + shipLen - 1 > setLength()) {
-        updateMsg('Ship cannot be outside of the board!')
-        return false
-      }
-      for (let j = 0; j < shipLen; j++) {
-        if (board[`${parseInt(x) + j},${y}`]) {
-          updateMsg('Cannot place ship on top of existing ship!')
-          return false
-        } else {
-          updateMsg('Ship placed!')
-          return true
-        }
-      }
-    }
+    return check(shipLen)
   }
 
   return { placeShip, board, receiveAttack, allSunk, checkAvail, computerSetup, ships }
